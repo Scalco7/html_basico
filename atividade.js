c37f1592b809b88d9910e2acba662fcc58796811
Felipe Scalco <fscalco7@gmail.com>
2025-04-28 16:18:49 -0300

    .

---
+++ atividade.js
@@ -0,0 +1,155 @@
+//Felipe Maciel Scalco - RA: 2565838
+//Raphael dos Santos Souza - RA: 2565951
+
+function ex_01() {
+
+}
+
+function ex_02() {
+    var idade = 25
+}
+
+function ex_03() {
+    
+}
+
+function ex_04() {
+    let nome = 'felipe'
+    let nome = 'rapahel'
+
+    //da erro
+}
+
+function ex_05() {
+    
+}
+
+function ex_06() {
+    let nome = 'felipe'
+    console.log("nome: " + nome)
+}
+
+function ex_07() {
+    
+}
+
+function ex_08(number) {
+    if (number % 2 == 0) {
+        console.log("Par")
+    } else {
+        console.log("impar")
+    }
+}
+
+function ex_09() {
+    
+}
+
+function ex_10(a, b) {
+    return a * b
+}
+
+function ex_11() {
+    
+}
+
+function ex_12() {
+    let x = 10
+    x = 10 + 5
+}
+
+function ex_13() {
+    
+}
+
+function ex_14(number) {
+    console.log(number < 0 ? "Negativo" : "Positivo")
+}
+
+function ex_15() {
+    
+}
+
+function ex_16() {
+    let nameList = ["Raphael", "Jefferson"]
+
+    nameList.push("Felipe Scalco")
+}
+
+function ex_17() {
+    
+}
+
+function ex_18() {
+    const obj = {
+        name: "Felipe",
+        years: 20
+    }
+}
+
+function ex_19() {
+    
+}
+
+function ex_20() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i)
+    }
+}
+
+function ex_21() {
+    
+}
+
+function ex_22() {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Sucesso");
+        }, 2000);
+    });
+
+    promise.then(valor => {
+        console.log(valor);
+    }).catch(erro => {
+        console.error(erro);
+    });
+}
+
+function ex_23() {
+    
+}
+
+function ex_24(logado) {
+    if (logado) {
+        console.log("Bem vindo")
+    }
+}
+
+function ex_25() {
+    
+}
+
+function ex_26() {
+    let name = prompt("Nome: ")
+    alert(name)
+}
+
+function ex_27() {
+    
+}
+
+function ex_28() {
+    const value = 100
+    value = 30
+
+    // Cannot assign to "value" because it is a constant
+}
+
+function ex_29() {
+    
+}
+
+function ex_30(idade) {
+    if (idade > 18) console.log("Maior de idade")
+    else console.log("Menor de idade")
+}
